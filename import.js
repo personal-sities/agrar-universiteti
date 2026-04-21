e440e54947e5147c094a34d3525ae695bfb9c7ca
personal-sities <umirbekovdide@gmail.com>
2026-04-21 15:51:24 +0500

    Create import.js

---
+++ import.js
@@ -0,0 +1,12 @@
+[
+  {
+    "title": "1) Skript bo‘yicha umumiy ma'lumot",
+    "summary": "Bu bosh qismda qisqa mazmun turadi.",
+    "details": "Bu yerda batafsil ma'lumot yoziladi.\nYangi qatordan ham foydalanish mumkin."
+  },
+  {
+    "title": "2) Yana bir ma'lumot",
+    "summary": "Qisqa ko‘rinish",
+    "details": "To‘liq izoh shu yerda bo‘ladi."
+  }
+]
